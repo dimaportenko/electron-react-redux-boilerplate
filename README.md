299beffd112d77a20eede14630549547bea4c2ba
Dmytro Portenko <dvportenko@gmail.com>
2016-05-07 15:04:16 +0300

    simple boilerplate files added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # electron-react-redux-boilerplate
 Simple boilerplate for Electron app with React Redux
+
+To start using 
+1. Install node.js modules - npm install
+2. Run - npm run-script watch 
+3. Run in second console tab - npm start (note: 2 point should be run)
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: ['webpack/hot/dev-server', './src/index.js'],
+    },
+    output: {
+        path: './public/built',
+        filename: 'bundle.js',
+        publicPath: 'http://localhost:8080/built/'
+    },
+    devServer: {
+        contentBase: './public',
+        publicPath: 'http://localhost:8080/built/'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
